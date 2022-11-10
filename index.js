a2827f1f4f401a54e181756e6dfb8958651d07a0
majharul224 <majharulislam2225@gmail.com>
2022-11-10 09:55:25 +0600

    mongodb counected serversite

--- index.js
+++ index.js
@@ -13,11 +13,11 @@ app.use(express.json());
 
 
 
-// // mongocunected
+// mongocunected
 
-// const uri = `mongodb+srv://
-// ${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.esgnv.mongodb.net/?retryWrites=true&w=majority`;
-// const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+const uri = `mongodb+srv://
+${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.esgnv.mongodb.net/?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
 
 
 
